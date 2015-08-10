9d0b2cf56d0d8e1300895cf843841d855db3085a
Felix Kiss <felix@felixkiss.com>
2015-08-10 16:15:54 +0200

    Install felixkiss/uniquewith-validator

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'ar',
 
     /*
     |--------------------------------------------------------------------------
@@ -143,6 +143,8 @@ return [
         App\Providers\AppServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
+        Felixkiss\UniqueWithValidator\UniqueWithValidatorServiceProvider::class,
 
     ],
 
